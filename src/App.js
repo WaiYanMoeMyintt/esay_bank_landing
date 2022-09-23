64e755beb4d33794b902f6b355177bfcdf9c39f1
WaiYanMoeMyint <wymoemyint4523@gmail.com>
2022-09-23 17:10:27 +0630

    Complete App js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import "./index.css";
+import Navbar from './Nav/Navbar';
+import Header from './Header/Header';
+import Wave from './Wave/Wave';
+import Service from './Service/Service';
+import Article from './Article/Article';
+import Footer from './Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Navbar/>
+         <Header />
+         <Wave />
+         <Service />
+         <Article />
+         <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
